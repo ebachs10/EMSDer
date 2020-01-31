99f8dc06f9b2e133f46bca71ab31df085952d9df
Esben Bach Soerensen <ebachs10@gmail.com>
2020-01-31 21:24:33 +0100

    Added first models in components

--- backend/emsder/emsder/components/admin.py
+++ backend/emsder/emsder/components/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Signal, Component, Manufacture
 
-# Register your models here.
+#class SignalAdmin(admin.ModelAdmin):
+
+#   fieldsets = [
+#       ("Name", {'fields': ["name"]}),
+#       ("Category", {'fields': ["category"]})
+#   ]
+#
+#class ComponentAdmin(admin.ModelAdmin):
+#
+#    fieldsets = [
+#        ("Type Number", {'fields': ["typenumber"]}),
+#        ("Signals", {'fields': ["Signals"]})
+#    ]
+
+
+class SignalInline(admin.StackedInline):
+    model = Signal
+
+class ComponentAdmin(admin.ModelAdmin):
+    inlines = [
+        SignalInline
+        
+    ]
+
+#admin.site.register(Signal, SignalAdmin)
+admin.site.register(Component, ComponentAdmin)
+#admin.site.register(Component)
+admin.site.register(Signal)
+admin.site.register(Manufacture)
--- backend/emsder/emsder/components/models.py
+++ backend/emsder/emsder/components/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Manufacture(models.Model):
+    name = models.CharField(max_length=200) 
+    #component = models.ForeignKey(Component, related_name='manufacturer', on_delete=models.CASCADE)    
+
+class SignalCategory(models.Model):
+    name = models.CharField(max_length=200) 
+    #component = models.ForeignKey(Component, related_name='manufacturer', on_delete=models.CASCADE)    
+
+class Component(models.Model):
+    typenumber = models.CharField(max_length=200)
+    manufacture = models.OneToOneField(Manufacture, default=1, on_delete = models.SET_DEFAULT)
+
+    def __str__(self):
+        return self.typenumber
+  
+
+class Signal(models.Model):
+    name = models.CharField(max_length=200)
+    component = models.ForeignKey(Component, related_name='signals', default=1, on_delete=models.CASCADE)
+    signalcategory = models.OneToOneField(SignalCategory, default=1, on_delete = models.SET_DEFAULT)
+
+    def __str__(self):
+        return self.name
+
+
