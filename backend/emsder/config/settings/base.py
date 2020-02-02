23f62969dcd2d363d478b65076df50c82b1f41fb
Esben Bach Soerensen <ebachs10@gmail.com>
2020-02-02 19:22:36 +0100

    Inital commit after failure. Lesson learned: Comit often

--- backend/emsder/config/settings/base.py
+++ backend/emsder/config/settings/base.py
@@ -75,8 +75,6 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "emsder.users.apps.UsersConfig",
     # Your stuff: custom apps go here
-    "emsder.components.apps.ComponentsConfig",
-    "emsder.basemodels.apps.BasemodelsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- backend/emsder/emsder/basemodels/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BasemodelsConfig(AppConfig):
-    name = 'emsder.basemodels'
