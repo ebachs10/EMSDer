aad05cc87b21ab1907264294b96c68c7678c31bd
Esben Bach Soerensen <ebachs10@gmail.com>
2020-02-07 23:52:56 +0100

    Got count to work.

--- backend/emsder/emsder/projects/admin.py
+++ backend/emsder/emsder/projects/admin.py
@@ -4,15 +4,15 @@ from reversion.admin import VersionAdmin
 from django.db.models import Count
 
 class ProjectAdmin(VersionAdmin):
-    list_display = ('name', 'show_subcontractor_count')
-
     def get_queryset(self, request):
         qs = super(ProjectAdmin, self).get_queryset(request)
-        return qs.annotate(subcontractor_count=Count('subcontractors'))
+        return qs.annotate(subcontractors_count=Count('subcontractor'))
 
-    def show_subcontractor_count(self, inst):
-        return inst.subcontractor_count
+    def subcontractors_count(self, inst):
+        return inst.subcontractors_count
 
-    show_subcontractor_count = 'subcontractor_count'
+
+    list_display = ['name', 'subcontractors_count']
+
 
 admin.site.register(Project, ProjectAdmin)
